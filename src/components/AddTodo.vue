2229a0d5fde241492555068ce98c4dc4b4f8675e
devinaharidas <devinaharidas@gmail.com>
2019-07-05 13:09:02 +1200

    second commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,61 @@
+<template>
+    <form @submit="addTodo">
+        
+
+        <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
+             
+        <input type="submit" value="Submit" class="btn">
+    </form>
+
+
+</template>
+<script>
+   // import uuid from 'uuid';
+    export default {
+        name: "AddTodo",
+        data() {
+            return {
+
+                title: ''
+            }
+
+
+        },
+        methods: {
+            addTodo(e) {
+                e.preventDefault();
+                const newTodo = {
+                    //id: uuid.v4(),
+                    title: this.title,
+                    completed:false
+
+
+                }
+                //send up to the parent
+                this.$emit('addTodo', newTodo);
+            }
+
+
+        } 
+
+    }
+
+
+
+</script>
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        flex: 2;
+    }
+
+
+</style>
